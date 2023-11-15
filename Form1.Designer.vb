c49558e4e65869b6a2ad16a55ee130abc2498c7a
Mohamed Alsaffar <mohammedalsaffar6767@gmail.com>
2023-11-15 17:53:18 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(214, 42)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(191, 332)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(335, 26)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 337)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Event Name"
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(451, 425)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 34)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 498)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(899, 324)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(836, 458)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 34)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Sort"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Daily", "Monthly", "Yearly"})
+        Me.ComboBox1.Location = New System.Drawing.Point(766, 458)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(64, 28)
+        Me.ComboBox1.TabIndex = 9
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.calendar_vb.My.Resources.Resources._4314_VisualBasic_v_rgb_ai_2_removebg_preview
+        Me.PictureBox1.Location = New System.Drawing.Point(579, 42)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(332, 253)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 399)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 20)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Time "
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(191, 393)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(335, 26)
+        Me.TextBox2.TabIndex = 13
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(836, 828)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 34)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Update"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.ForeColor = System.Drawing.Color.Red
+        Me.Button5.Location = New System.Drawing.Point(736, 828)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 34)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Del"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(196, 370)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(175, 20)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Format : hh:mm AM/PM"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(836, 301)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 34)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "r"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Daily", "Monthly", "Yearly"})
+        Me.ComboBox2.Location = New System.Drawing.Point(687, 458)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(64, 28)
+        Me.ComboBox2.TabIndex = 18
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Daily", "Monthly", "Yearly"})
+        Me.ComboBox3.Location = New System.Drawing.Point(617, 458)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(64, 28)
+        Me.ComboBox3.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkSalmon
+        Me.ClientSize = New System.Drawing.Size(923, 891)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+    Private dataTable As New DataTable()
+    Private dt As New DataTable()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dateValue As Date = MonthCalendar1.SelectionStart.Date
+        Dim timeValue As String = DateTime.Now.ToString("hh:mm tt")
+        Dim eventValue As String = TextBox1.Text
+
+        dataTable.Rows.Add(dateValue, timeValue, eventValue)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dataTable.Columns.Add("Date", GetType(Date))
+        dataTable.Columns.Add("Time", GetType(String))
+        dataTable.Columns.Add("Event", GetType(String))
+        TextBox2.Hide()
+        Label2.Hide()
+        Label3.Hide()
+
+
+        DataGridView1.DataSource = dataTable
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label2.Show()
+        TextBox2.Show()
+        Label3.Show()
+
+        MessageBox.Show("Please Select The Row Then Type New Value in TextBox Above")
+
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim selectedRow = DataGridView1.SelectedRows(0)
+            Dim rowIndex = DataGridView1.Rows.IndexOf(selectedRow)
+
+
+            dataTable.Rows(rowIndex)("Time") = TextBox2.Text
+            dataTable.Rows(rowIndex)("Event") = TextBox1.Text
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim selectedRow = DataGridView1.SelectedRows(0)
+            Dim rowIndex = DataGridView1.Rows.IndexOf(selectedRow)
+
+            ' Delete the row from the DataTable
+            dataTable.Rows.RemoveAt(rowIndex)
+        End If
+    End Sub 'The expression contains undefined function call Month().'
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim selectedOption As String = ComboBox1.SelectedItem.ToString()
+        Dim selectedDate As Date = MonthCalendar1.SelectionStart.Date
+
+        Select Case selectedOption
+            Case "Daily"
+                dataTable.DefaultView.RowFilter = $"Date = #{selectedDate.ToString("MM/dd/yyyy")}#"
+            Case "Monthly"
+                dataTable.DefaultView.RowFilter = $"Date >= #{selectedDate.ToString("MM/01/yyyy")}# AND Date < #{selectedDate.AddMonths(1).ToString("MM/01/yyyy")}#"
+            Case "Yearly"
+                dataTable.DefaultView.RowFilter = $"Date >= #{selectedDate.ToString("01/01/yyyy")}# AND Date < #{selectedDate.AddYears(1).ToString("01/01/yyyy")}#"
+        End Select
+
+        DataGridView1.DataSource = dataTable.DefaultView
+    End Sub
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        dt = dataTable
+        DataGridView1.DataSource = dt
+    End Sub
+End Class
